32c0fc6d5ed935bab4c37ba19c2197f669719e50
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2021-07-13 17:02:01 -0300

    Create main.js

---
+++ nwl/main.js
@@ -0,0 +1,30 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+const links = document.querySelectorAll('nav ul li a')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* sombreado header */
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+    // scroll for maior que a altura do header
+  } else {
+    header.classList.remove('scroll')
+    // quando o scroll for menor que o header
+  }
+})
