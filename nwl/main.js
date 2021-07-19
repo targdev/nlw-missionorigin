ac053c5fab6874de7e26c89a5d8977123aead1da
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2021-07-19 20:32:54 -0300

    Update main.js

--- nwl/main.js
+++ nwl/main.js
@@ -15,11 +15,10 @@ for (const link of links) {
 }
 
 /* sombreado header */
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
     // scroll for maior que a altura do header
@@ -27,7 +26,7 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
     // quando o scroll for menor que o header
   }
-})
+}
 
 /* swiper */
 const swiper = new Swiper('.swiper-container', {
@@ -37,4 +36,36 @@ const swiper = new Swiper('.swiper-container', {
   },
   mousewheel: true,
   keyboard: true
+})
+
+/* scroll reveal */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 900,
+  reset: true
+})
+
+scrollReveal.reveal(
+  '#home .image, #home .text, #about .image, #about .text, #services header, #services .card, #testimonials header, #testimonials .testimonials, #contact .text, #contact .contact, footer .brand, footer .social',
+  {
+    interval: 100
+  }
+)
+
+/* back to top */
+
+function backToBackTop() {
+  const backToBackTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 600) {
+    backToBackTopButton.classList.add('show')
+  } else {
+    backToBackTopButton.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToBackTop()
 })
